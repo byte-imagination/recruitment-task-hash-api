855e003f3e5e4418a63a4de9e0927aa2e27a55e6
Roger Zacharczyk <roger.zacharczyk@gmail.com>
2022-01-21 09:36:39 +0100

    Initial commit

---
+++ src/main/java/recruitment/hashapi/HashApiApplication.java
@@ -0,0 +1,13 @@
+package recruitment.hashapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HashApiApplication.class, args);
+  }
+
+}
